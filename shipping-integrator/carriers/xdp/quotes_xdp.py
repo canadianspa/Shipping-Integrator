f96394505e422da0ba511b2500ae9e23baa54c35
Jasper <jasperhaward@virginmedia.com>
2020-11-20 14:20:56 +0000

    XDP file struct

---
+++ shipping-integrator/carriers/xdp/quotes_xdp.py
@@ -0,0 +1,14 @@
+from classes.service import Service
+from .accounts import (
+    XDP_A,
+    XDP_B,
+    XDP_C,
+    XDP_A_KEY,
+    XDP_B_KEY,
+    XDP_C_KEY,
+)
+
+
+def build_quotes_xdp():
+    return [
+    ]
