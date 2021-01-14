777db76d73d785f8c72d243bef9e2255b14dc0e0
Jasper <jasperhaward@virginmedia.com>
2021-01-14 11:36:21 +0000

    Add additional tracking number type

--- carriers/utils.py
+++ carriers/utils.py
@@ -14,11 +14,15 @@ def get_carrier(tracking_number):
     # Get carrier using first char of TrackingNo
     # Z  => XDP
     # L  => DX 1man
-    # N  => DX 2man
+    # N/G  => DX 2man
 
     if tracking_number[0] == "Z":
         return "xdp"
-    elif tracking_number[0] == "L" or tracking_number[0] == "N":
+    elif (
+        tracking_number[0] == "L"
+        or tracking_number[0] == "N"
+        or tracking_number[0] == "G"
+    ):
         return "dx"
     else:
         raise ValueError(f"Unknown carrier for: {tracking_number}")
